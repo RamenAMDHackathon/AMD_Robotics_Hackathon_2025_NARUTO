a758f6603ebe3f2fc85c72282c23922828762155
RozeSullatui <roze23vpa40769@gmail.com>
2025-12-07 15:39:48 +0900

    add:yolo_training_model

---
+++ mission2/code/yolo_training/yolo_training_all.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+# YOLOv11n
+# model_n = YOLO("yolo11n.pt")
+# model_n.train(data="./yolo_dataset_all/data.yaml", epochs=100, imgsz=640, batch=4,freeze=10,lr0=0.001, name="yolo11n_bottle_cup_all")
+# model_n.save("yolo11n_bottle_cup_all.pt")
+
+# YOLOv11s
+model_s = YOLO("yolo11s.pt")
+result_s = model_s.train(data="./yolo_dataset_all/data.yaml", epochs=100, imgsz=640, batch=4,freeze=10,lr0=0.001, name="yolo11s_bottle_cup_all")
+model_s.save("yolo11s_bottle_cup_all.pt")
+
+# model_s = YOLO("yolo11m.pt")
+# result_s = model_s.train(data="./yolo_dataset_all/data.yaml", epochs=100, imgsz=1280, batch=4,freeze=10,lr0=0.001, name="yolo11m_bottle_cup_all")
+# model_s.save("yolo11m_bottle_cup_all.pt")
